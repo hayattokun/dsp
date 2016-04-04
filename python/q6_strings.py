1a8ad787265f3f8deca3a37a36bc88c6f58d19c8
hayattokun <hyatt.bailey@gmail.com>
2016-04-04 16:46:18 -0400

    Update q6_strings.py

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -123,6 +123,13 @@ def verbing(s):
     >>> verbing('do')
     'do'
     """
+    if len(s) >= 3:
+        if s[-3:] == 'ing':
+            return s + 'ly'
+        else:
+            return s + 'ing'
+    else:
+        return s
     raise NotImplementedError
 
 
