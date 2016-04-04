5531791336ee0d6b533beef6e2532e8078ca296d
hayattokun <hyatt.bailey@gmail.com>
2016-04-04 17:50:01 -0400

    Update q6_strings.py

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -173,4 +173,24 @@ def front_back(a, b):
     >>> front_back('Kitten', 'Donut')
     'KitDontenut'
     """
+    a_front = ''
+    b_front = ''
+    a_back = ''
+    b_back = ''
+    
+    if len(a) % 2 == 0:
+        a_front = a[:len(a) / 2]
+        a_back = a[len(a) / 2:]
+    else:
+        a_front = a[:len(a) / 2 + 1]
+        a_back = a[len(a) / 2 + 1:]
+    
+    if len(b) % 2 == 0:
+        b_front = b[:len(b) / 2]
+        b_back = b[len(b) / 2:]
+    else:
+        b_front = b[:len(b) / 2 + 1]
+        b_back = b[len(b) / 2 + 1:]
+    
+    return a_front + b_front + a_back + b_back
     raise NotImplementedError
